ff7790e1e0dd869e319fafe10161b18352c9644e
=Rahul lodhi <146419777+RAHUL676789@users.noreply.github.com>
2024-12-28 22:03:38 +0530

    new changes in session

--- Server/index.js
+++ Server/index.js
@@ -73,7 +73,6 @@ const sessionOptions = {
     expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
     maxAge: 7 * 24 * 60 * 60 * 1000,
     httpOnly: true,
-    secure:true,
     secure: process.env.NODE_ENV === "production",
     sameSite:"none",
     
