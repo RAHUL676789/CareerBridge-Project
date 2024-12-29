9d1c1be8a3fcaac370a6529c5305fc1385923f7d
=Rahul lodhi <146419777+RAHUL676789@users.noreply.github.com>
2024-12-29 13:00:01 +0530

    proxy set

--- Server/index.js
+++ Server/index.js
@@ -75,7 +75,7 @@ const sessionOptions = {
     expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
     maxAge: 7 * 24 * 60 * 60 * 1000,
     httpOnly: true,
-    secure: true,
+    secure: false,
     sameSite:false,
     
   },
